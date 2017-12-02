29df113d948bd4b21d0b23cea173ea0f9f004953
DESKTOP-KD7EG8O\sugit <sugitaw95@gmail.com>
2017-12-03 01:34:00 +0800

    New

---
+++ app/src/test/java/com/example/iak/wallmaterialcalculator/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created by Sugita Wiguna on 12/3/17 12:34 AM
+ * Copyright (c) 2017. All rights reserved.
+ *
+ * Last modified 12/2/17 10:08 PM
+ */
+
+package com.example.iak.wallmaterialcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
